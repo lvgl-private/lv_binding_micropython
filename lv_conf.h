089d01c77c668e89953e99ce0176884fdacaa348
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-01-31 14:18:18 +0100

    fix conflicts

--- lv_conf.h
+++ lv_conf.h
@@ -129,10 +129,7 @@
  *With complex image decoders (e.g. PNG or JPG) caching can save the continuous open/decode of images.
  *However the opened images might consume additional RAM.
  *0: to disable caching*/
-<<<<<<< HEAD
-#define LV_IMG_CACHE_DEF_SIZE 16
-=======
-#define LV_IMG_CACHE_DEF_SIZE   1
+#define LV_IMG_CACHE_DEF_SIZE   16
 
 /*Number of stops allowed per gradient. Increase this to allow more stops.
  *This adds (sizeof(lv_color_t) + 1) bytes per additional stop*/
@@ -155,7 +152,6 @@
      *The increase in memory consumption is (24 bits * object's width)*/
     #define LV_DITHER_ERROR_DIFFUSION   0
 #endif
->>>>>>> efd296ffce9f5b0e2e51769f09aa4502afe39b55
 
 /*Maximum buffer size to allocate for rotation.
  *Only used if software rotation is enabled in the display driver.*/
@@ -205,7 +201,6 @@
 #define LV_USE_LOG 1
 #if LV_USE_LOG
 
-<<<<<<< HEAD
 /*How important log should be added:
  *LV_LOG_LEVEL_TRACE       A lot of logs to give detailed information
  *LV_LOG_LEVEL_INFO        Log important events
@@ -228,30 +223,6 @@
 #  define LV_LOG_TRACE_OBJ_CREATE 1
 #  define LV_LOG_TRACE_LAYOUT     1
 #  define LV_LOG_TRACE_ANIM       1
-=======
-    /*How important log should be added:
-    *LV_LOG_LEVEL_TRACE       A lot of logs to give detailed information
-    *LV_LOG_LEVEL_INFO        Log important events
-    *LV_LOG_LEVEL_WARN        Log if something unwanted happened but didn't cause a problem
-    *LV_LOG_LEVEL_ERROR       Only critical issue, when the system may fail
-    *LV_LOG_LEVEL_USER        Only logs added by the user
-    *LV_LOG_LEVEL_NONE        Do not log anything*/
-    #define LV_LOG_LEVEL LV_LOG_LEVEL_WARN
-
-    /*1: Print the log with 'printf';
-    *0: User need to register a callback with `lv_log_register_print_cb()`*/
-    #define LV_LOG_PRINTF 0
-
-    /*Enable/disable LV_LOG_TRACE in modules that produces a huge number of logs*/
-    #define LV_LOG_TRACE_MEM        1
-    #define LV_LOG_TRACE_TIMER      1
-    #define LV_LOG_TRACE_INDEV      1
-    #define LV_LOG_TRACE_DISP_REFR  1
-    #define LV_LOG_TRACE_EVENT      1
-    #define LV_LOG_TRACE_OBJ_CREATE 1
-    #define LV_LOG_TRACE_LAYOUT     1
-    #define LV_LOG_TRACE_ANIM       1
->>>>>>> efd296ffce9f5b0e2e51769f09aa4502afe39b55
 
 #endif  /*LV_USE_LOG*/
 
